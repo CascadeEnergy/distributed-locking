9b40eaa04c7f44f208ddb8d6f6d922b868f87ced
Charles Berube <charles.berube@cascadeenergy.com>
2015-07-20 12:58:55 -0700

    Unit tests and travis.yml

--- tests/DistributedLocking/LockSet/DefaultLockSetFactoryTest.php
+++ tests/DistributedLocking/LockSet/DefaultLockSetFactoryTest.php
@@ -1,7 +1,30 @@
 <?php
 namespace Tests\DistributedLocking\LockSet;
 
+use Cascade\DistributedLocking\ILockProvider;
+use Cascade\DistributedLocking\LockSet\DefaultLockSetFactory;
+use Cascade\DistributedLocking\LockSet\ILockSetFactory;
+
 class DefaultLockSetFactoryTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var ILockProvider */
+    private $lockProvider;
 
+    /** @var ILockSetFactory */
+    private $defaultLockSetFactory;
+
+    public function setUp()
+    {
+        $this->lockProvider = $this->getMock('Cascade\\DistributedLocking\\ILockProvider');
+
+        $this->defaultLockSetFactory = new DefaultLockSetFactory($this->lockProvider);
+    }
+
+    public function testItShouldCreateADefaultLockSetWithTheGivenLockProvider()
+    {
+        $lockSet = $this->defaultLockSetFactory->createLockSet();
+
+        $this->assertInstanceOf('Cascade\\DistributedLocking\\LockSet\\DefaultLockSet', $lockSet);
+        $this->assertAttributeSame($this->lockProvider, 'lockProvider', $lockSet);
+    }
 }
---
+++ tests/DistributedLocking/LockSet/DefaultLockSetTest.php
@@ -0,0 +1,85 @@
+<?php
+namespace Tests\DistributedLocking\LockSet;
+
+use Cascade\DistributedLocking\ILockProvider;
+use Cascade\DistributedLocking\LockSet\DefaultLockSet;
+use Cascade\DistributedLocking\LockSet\ILockSet;
+
+class DefaultLockSetTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \PHPUnit_Framework_MockObject_MockObject */
+    private $lockProvider;
+
+    /** @var ILockSet */
+    private $lockSet;
+
+    public function setUp()
+    {
+        $this->lockProvider = $this->getMock('Cascade\\DistributedLocking\\ILockProvider');
+
+        /** @noinspection PhpParamsInspection */
+        $this->lockSet = new DefaultLockSet($this->lockProvider);
+    }
+
+    public function testItShouldAcquireSingleLocksWhenRequested()
+    {
+        $this->acquireTestLocks();
+    }
+
+    public function testItShouldReturnFalseWhenALockCannotBeAcquired()
+    {
+        $this->lockProvider
+            ->expects($this->once())
+            ->method('lock')
+            ->willReturn(false);
+
+        $this->assertFalse($this->lockSet->lock('foo'));
+    }
+
+    public function testItShouldReleaseAllPreviouslyAcquiredLockedWhenRequested()
+    {
+        $this->acquireTestLocks();
+
+        $this->lockProvider
+            ->expects($this->exactly(3))
+            ->method('release')
+            ->withConsecutive(['fooHandle'], ['barHandle'], ['bazHandle']);
+
+        $this->lockSet->releaseAll();
+    }
+
+    public function testItShouldReturnTrueWhenAllLocksAreValid()
+    {
+        $this->acquireTestLocks();
+
+        $this->lockProvider
+            ->expects($this->exactly(3))
+            ->method('isValid')
+            ->withConsecutive(['foo', 'fooHandle'], ['bar', 'barHandle'], ['baz', 'bazHandle'])
+            ->willReturn(true);
+
+        $this->assertTrue($this->lockSet->isValid());
+    }
+
+    public function testItShouldReturnFalseWhenAnyLockIsInvalid()
+    {
+        $this->lockProvider->expects($this->once())->method('lock')->with('foo')->willReturn('fooHandle');
+        $this->lockProvider->expects($this->once())->method('isValid')->with('foo', 'fooHandle')->willReturn(false);
+
+        $this->lockSet->lock('foo');
+        $this->assertFalse($this->lockSet->isValid());
+    }
+
+    private function acquireTestLocks()
+    {
+        $this->lockProvider
+            ->expects($this->exactly(3))
+            ->method('lock')
+            ->withConsecutive(['foo'], ['bar'], ['baz'])
+            ->willReturnOnConsecutiveCalls('fooHandle', 'barHandle', 'bazHandle');
+
+        $this->assertTrue($this->lockSet->lock('foo'));
+        $this->assertTrue($this->lockSet->lock('bar'));
+        $this->assertTrue($this->lockSet->lock('baz'));
+    }
+}
